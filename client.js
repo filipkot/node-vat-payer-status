c7dda4b03dd581693af50817eef04a868dfbd290
Tomasz Filipkowski <filipkow@wit.edu.pl>
2018-09-07 09:07:12 +0200

    Inital

---
+++ client.js
@@ -0,0 +1,33 @@
+var soap = require('soap');
+var url = 'https://sprawdz-status-vat.mf.gov.pl/?wsdl';
+
+module.exports = {
+    SprawdzNIP: (nip) => {
+        if(!validateNip(nip))
+            return 'Błędny NIP';
+
+        var args = { NIP: nip };
+        soap.createClient(url, (err, client) => {
+            client.SprawdzNIP(args, (err, result) => {
+                return result.Komunikat;
+            });
+        });
+    }
+};
+
+// zrodlo: http://blog.aleksander.kaweczynski.pl/walidacja-numerow-pesel-nip-regon-w-javascript-i-php/ 
+var validateNip = (nip) => {
+    var nipWithoutDashes = String(nip).replace(/-/g,'');
+    var reg = /^[0-9]{10}$/;
+    if(!reg.test(nipWithoutDashes)) {
+        return false;
+    }
+    else{
+        var digits = nipWithoutDashes.split('');
+        var checksum = (6*parseInt(digits[0]) + 5*parseInt(digits[1]) + 7*parseInt(digits[2]) + 
+            2*parseInt(digits[3]) + 3*parseInt(digits[4]) + 4*parseInt(digits[5]) + 
+            5*parseInt(digits[6]) + 6*parseInt(digits[7]) + 7*parseInt(digits[8]))%11;
+         
+        return (parseInt(digits[9])==checksum);
+    }
+}
